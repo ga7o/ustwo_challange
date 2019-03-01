b3150ae507b01831503d9c460b9fc81327233cbf
ruialves <rui@kinsu.co.uk>
2019-03-01 17:03:18 +0000

    Setting up the frontend structure for PikaAPI SPA

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { ItemsComponent } from './components/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PokemonComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
